371e4778bd10fe0278e18ed3f75ec86248b9e79b
BlakeVasista-1 <155288962+BlakeVasista-1@users.noreply.github.com>
2024-07-23 10:46:22 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -2,10 +2,13 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://shashank:vasista@cluster0.xt8dcep.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
-        console.log('Mongodb connected');
+        await mongoose.connect('mongodb+srv://shashankvasista0:3z2GbJGilfXVDOID@cluster0.xt8dcep.mongodb.net/movieRecommender?retryWrites=true&w=majority&appName=Cluster0', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('MongoDB connected');
     } catch (error) {
-        console.error(error);
+        console.error('Error connecting to MongoDB:', error);
         process.exit(1);
     }
 };
@@ -13,15 +16,18 @@ const connectDB = async () => {
 const userInputSchema = new mongoose.Schema({
     keywords: {
         type: String,
-        required:true
+        required: true
     }
 });
 
 const movieTitleSchema = new mongoose.Schema({
-    title: String,
+    title: {
+        type: String,
+        required: true
+    }
 });
 
-const collections = mongoose.model('userInputs', userInputSchema);
-const collections1 = mongoose.model('recommendedMovieNames', movieTitleSchema);
+const Searches = mongoose.model('Searches', userInputSchema);
+const Recommendations = mongoose.model('Recommendations', movieTitleSchema);
 
-module.exports = { connectDB, collections, collections1};
+module.exports = { connectDB, Searches, Recommendations };
